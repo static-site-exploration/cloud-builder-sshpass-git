23a8ce7dd9a0348d17edd8e284597ed3b53ed7ab
Mark Dorrill <mark.dorrill@gmail.com>
2018-03-11 19:42:51 +0000

    Update entrypoint.sh
    Setup basic command with checks for variable and git command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-export SSHPASS=$build_ssh_password;
+export SSHPASS=$ssh_password;
 
-sshpass -e git
+if [[ -z "$SSHPASS" ]]; then
+  echo "No password provided";
+  exit 1;
+fi
+
+if [[ -z "$@" ]]; then
+  echo "No git command provided, so will show status";
+  1="status";
+fi
+
+sshpass -e git "$@" 
