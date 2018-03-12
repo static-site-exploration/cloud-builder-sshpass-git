a32180f94d2c94c9f1b89339260d93da506e845e
Mark Dorrill <mark.dorrill@gmail.com>
2018-03-12 13:06:16 +0000

    Update sshpass-wrapped-git.bash
    Added simple ssh test options

--- sshpass-wrapped-git.bash
+++ sshpass-wrapped-git.bash
@@ -4,9 +4,19 @@ set -e
 # Arguments have not been provided
 if [[ -z "$@" ]]; then
   
-  echo "No git command provided, so will display git version";
+  echo "No command provided, so will display git version";
   exec git version;
+
+# Test ssh with sshpass
+elif [[ "$1" == "ssh" ]] && [[ "$2" == "sshpass" ]] && [[ -n "$ssh_password" ]]; then
   
+  exec sshpass -p $ssh_password ssh git@gitub.com;
+
+# Test ssh without sshpass
+elif [[ "$1" == "ssh" ]] && [[ - z "$2" ]] && [[ -n "$ssh_password" ]]; then
+  
+  ssh git@gitub.com;
+
 # Switch between plain git and sshpass git based on the existence (or not) of the sshpass envionmnet variable
 # Arguments are supplied and the ssh variable is set
 elif [[ -n "$@" ]] && [[ -n "$ssh_password" ]]; then
