bf23373e45fa5cf0b11ed5bf099f83f96ae2b34e
Mark Dorrill <mark.dorrill@gmail.com>
2018-03-12 01:18:35 +0000

    Update sshpass-wrapped-git.bash
    Minimal script working, trying sshpass conditionals in this commit

--- sshpass-wrapped-git.bash
+++ sshpass-wrapped-git.bash
@@ -1,6 +1,31 @@
 #!/bin/bash
 set -e
 
-echo "Running with parameters: " && echo "$@";
+if [[ -d "$ssh_password" ]]; then
 
-exec git "$@";
+  export SSHPASS=$ssh_password;
+  
+  if [[ -z "$@" ]]; then
+  
+    echo "No git command provided, so will use git status";
+    1="status";
+    
+  fi
+  
+  exec sshpass -e git "$@"
+
+elif [[ -z "$ssh_password" ]]; then
+
+  if [[ -z "$@" ]]; then
+  
+    echo "No git command provided, so will use git status";
+    1="status";
+    
+  fi
+  
+  echo "No password provided, so will run git on its own... \n";
+  echo "Running Git WIHTOUT SSHPASS! "; 
+  echo "Using parameters: " && echo "$@";
+  exec git "$@";
+  
+fi
