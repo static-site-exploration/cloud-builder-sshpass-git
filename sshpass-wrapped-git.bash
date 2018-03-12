794d1c0048d655a1dfba4c5a73d7ea6d1ad2e98c
Mark Dorrill <mark.dorrill@gmail.com>
2018-03-12 12:17:51 +0000

    Update sshpass-wrapped-git.bash
    Removed conditional nesting and corrected d flags to n (non-empty)

--- sshpass-wrapped-git.bash
+++ sshpass-wrapped-git.bash
@@ -1,34 +1,32 @@
 #!/bin/bash
 set -e
 
-# Check if any arguments have been provided
+# Arguments have not been provided
 if [[ -z "$@" ]]; then
   
-  echo "No git command provided, so will use git status";
-  exec git status;
+  echo "No git command provided, so will display git version";
+  exec git version;
+  
+# Switch between plain git and sshpass git based on the existence (or not) of the sshpass envionmnet variable
+# Arguments are supplied and the ssh variable is set
+elif [[ -n "$@" ]] && [[ -n "$ssh_password" ]]; then
+
+  export SSHPASS=$ssh_password;
+  
+  echo "Running git with sshpas... ";
+  echo "Using parameters: " && echo "$@";
+  exec sshpass -e git "$@"
+
+# Arguments are supplied without the ssh variable being set
+elif [[ -n "$@" ]] && [[ -z "$ssh_password" ]]; then
+
+  echo "Running git WIHTOUT SSHPASS...";
+  echo "Using parameters: " && echo "$@";
+  exec git "$@";
 
 else
 
-  echo "Using parameters: " && echo "$@";
-  
-  # Switch between plain git and sshpass git based on the existence (or not) of the sshpass envionmnet variable
-  if [[ -d "$ssh_password" ]] && [[ -d "$@" ]]; then
-
-    export SSHPASS=$ssh_password;
-
-    echo "Running git with sshpas... "; 
-    exec sshpass -e git "$@"
-
-  elif [[ -z "$ssh_password" ]] && [[ -d "$@" ]]; then
-
-    echo "Running git WIHTOUT SSHPASS..."; 
-    exec git "$@";
-  
-  else
-  
-    echo "Error with git-sshpass";
-    exit 1;
-
-  fi
+  echo "Error with git-sshpass";
+  exit 1;
 
 fi
